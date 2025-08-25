004c5c0c91961fcd55194e7b87310c3712020a8c
Amir Khosravi <amirkhosravi921@gmail.com>
2025-08-25 15:04:55 +0330

    Connect to Mongo

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
